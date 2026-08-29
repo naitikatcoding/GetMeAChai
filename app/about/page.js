4ff4c8193a5ea2454b8ed2692628534b41ae5dc9
NAITIK GUPTA <naitikgupta2713@gmail.com>
2026-08-29 09:56:41 +0530

    Completed About us and contact route

---
+++ app/about/page.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="text-center mx-9">
+        <p className="text-4xl font-extrabold py-16">About Us</p>
+        <div className="flex flex-col gap-15">
+          <p className="font-bold">
+            &quot;Fueling Creativity, One Cup at a Time&quot;
+          </p>
+          <p>
+            At Get Me A Chai, we are committed to fostering a vibrant community
+            where developers, creators, and influencers can thrive. Our
+            crowdfunding platform is designed to connect talented individuals
+            with supporters who believe in their vision and want to help bring
+            their projects to life. We understand that financial support is
+            crucial for creativity and innovation, and we strive to make the
+            process seamless and rewarding for both creators and backers.
+          </p>
+          <p>
+            Our mission is to empower developers, content creators, artists, and
+            influencers by providing them with the resources they need to
+            succeed. Whether you&apos;re a developer working on the next
+            groundbreaking app, a content creator producing engaging and
+            informative videos, an artist creating stunning visual works, or an
+            influencer sharing your unique perspective with the world, Get Me A
+            Chai is here to support you every step of the way.
+          </p>
+          <p>
+            We believe in the transformative power of community support and the
+            magic that happens when people come together to champion a common
+            cause. Our platform is more than just a crowdfunding site; it's a
+            space where dreams can become reality, and innovative ideas can
+            flourish. By providing a straightforward and user-friendly way for
+            patrons to contribute, we aim to create a supportive ecosystem that
+            nurtures creativity and drives progress.
+          </p>
+          <p>
+            At Get Me A Chai, we take pride in helping creators reach their full
+            potential. We are passionate about leveraging the power of
+            technology to build a community that values and invests in creative
+            endeavors. Join us on this exciting journey and be part of a
+            movement that celebrates and sustains the talents of developers,
+            creators, and influencers. Together, let&apos;s fuel creativity, one
+            cup at a time.
+          </p>
+        </div>
+      </div>
+
+      <div className="bg-gray-600 w-screen h-1 mt-16.5"></div>
+
+      <div className="text-center my-24">
+        <p>Why Choose Us</p>
+        <p>Support Creativity and Innovation</p>
+        <p>
+          At Get Me A Chai, we empower developers, creators, and influencers by
+          connecting them with supporters to fund their projects. No technical
+          skills required - our intuitive platform makes it easy to receive
+          support and bring your visions to life.
+        </p>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/contact/page.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="flex flex-col text-center justify-center my-20">
+        <span className="font-extrabold text-4xl mb-1">Contact Us</span>
+        <span className="font-bold text-2xl mb-20">
+          We&apos;d love to hear from you!
+        </span>
+        <span className="mb-8">
+          If you have any questions, feedback, or need support, feel free to
+          reach out to us:
+        </span>
+        <span>Email: support@getmeachai.com</span>
+        <span>Phone: +1-800-123-4567</span>
+        <span className="mb-8">
+          Address: 123 Creator Lane, Innovation City, CA 90210
+        </span>
+        <span className="mb-2">
+          Follow us on social media for the latest updates and news:
+        </span>
+        <div className="text-yellow-300 flex flex-col gap-5">
+          <a href="https://github.com/naitikatcoding" target="_blank">
+            Github
+          </a>
+          <a href="https://www.linkedin.com/in/naitikatwork/" target="_blank">
+            Linkedin
+          </a>
+          <a href="https://www.instagram.com/codedbynaitik/" target="_blank">
+            Instagram
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
