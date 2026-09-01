4ff3ea27bb1b937718746743c0846e5bce6640bb
NAITIK GUPTA <naitikgupta2713@gmail.com>
2026-09-01 20:20:18 +0530

    dashboard changes

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -12,12 +12,17 @@ const Username = async ({ params }) => {
         priority
         unoptimized
         className="rounded-lg object-cover w-full" />
-        <div className="absolute top-90 right-180"> 
+        <div className="absolute top-90 right-178"> 
           <Image src={"https://cdn.britannica.com/11/190811-050-4875CAA7/Sheryl-Lee-Laura-Palmer-Twin-Peaks.jpg"} alt="user pfp"
-        width={80}
-        height={120}
+        width={100}
+        height={130}
         priority
-        className="rounded-lg" />
+        className="rounded-full" />
+        </div>
+        <div className="flex flex-col text-center mt-25">
+          <span className="font-bold text-sm">&#64;Naitikatartistery</span>
+          <span className="text-slate-400">Create Digital Art Live</span>
+          <span className="text-slate-400">currently | 9,180 | members.</span>
         </div>
       </div>
     </>
