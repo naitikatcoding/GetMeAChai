3d7dd9b698ffb6978c8a3f5fad27a2019e9b9ff8
NAITIK GUPTA <naitikgupta2713@gmail.com>
2026-09-02 22:22:35 +0530

    mongoose install and connect

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
@@ -8,11 +8,29 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = true;
+
+      if (isAllowedToSignIn) {
+        if (account.provider == "github") {
+          const client = await mongoose.connect();
+
+          // const currentUser = await client
+          //   .db("user")
+          //   .collection("users")
+          //   .findOne({ email: email });
+        }
+      }
+    },
+  },
 
   secret: process.env.NEXTAUTH_SECRET,
 };
